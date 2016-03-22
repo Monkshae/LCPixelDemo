b232d70ddd070bd08b3d2f0c8510eca7e68a92fc
licong <1240690490@qq.com>
2016-03-22 11:36:15 +0800

    Initial Commit

---
+++ LCPixel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCPixel
+//
+//  Created by licong on 16/3/22.
+//  Copyright © 2016年 Sean Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCPixel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LCPixel
+//
+//  Created by licong on 16/3/22.
+//  Copyright © 2016年 Sean Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
